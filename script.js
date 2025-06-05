ef0a81facdff627013d4060154df94be2538f2e8
Wellington Ferreira de Lima Junior <wellingtonjr@isesc.local>
2025-06-05 20:59:19 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function Req(){
+    fetch("https://httpbin.org/html")
+        .then(resp => {
+            if(resp.status != 200){
+                throw new Error("Problemas no sevidor")
+            }
+            return resp.text();
+        })
+        .then(text => {
+            let d = new DOMParser();
+            let doc = d.parseFromString(text,"text/html");
+            console.log(doc)
+            extrairElemento(doc)
+        })
+        .catch(err => {
+            document.querySelector("#res").innerHTML = err.message;
+        })
+}
+
+function extrairElemento(doc){
+    let div = document.querySelector("#res");
+    let conteudo = doc.querySelectorAll("h1, p")
+    conteudo.forEach(el => {
+        let p = document.createElement("p");
+        p.textContent = el.textContent;
+        div.appendChild(p);
+    });
+}
+
+function main(){
+    document.querySelector("#btn").addEventListener("click",Req)
+}
+
+window.onload = main
+
+  
